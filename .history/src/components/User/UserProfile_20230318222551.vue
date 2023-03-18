6cf20357eb599cd57182c83bcb7c54c36585b24a
oderinde300 <oderinde.oluwatobiloba@gmail.com>
2023-03-18 22:27:26 +0100

    added a btn to nested routes

---
+++ .history/src/components/User/UserProfile_20230318222551.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="mt-8">User Profile</h1>
+  <h1 class="mt-8">User Profile</h1>
+
+  <h1 class="mt-8">User Profile</h1>
+  <h1 class="mt-8">User Profile</h1>
+  <h1 class="mt-8">User Profile</h1>
+</template>
+
+<script>
+export default {
+  name: "UserProfile",
+};
+</script>
